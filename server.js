c3ad5059c077e7a420856539f2b8869c9cd48a8e
JuanSeVillamil <zhukkako772@gmail.com>
2025-05-25 13:00:40 -0500

    Actualización de datos

--- server.js
+++ server.js
@@ -3,18 +3,29 @@ const express = require('express');
 const session = require('express-session');
 const path = require('path');
 const { Pool } = require('pg');
+const pgSession = require('connect-pg-simple')(session);
+
+app.use(session({
+  store: new pgSession({
+    pool: pool,                // usa el mismo pool de PostgreSQL
+    tableName: 'session'       // puedes cambiar el nombre si quieres
+  }),
+  secret: 'clave_secreta_segura',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 días
+}));
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Conexión a PostgreSQL
 const pool = new Pool({
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Middleware
